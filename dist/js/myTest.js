41a98b85c6363e3743b9207b6ab47c17598b7dc0
Chris oeps <oepschris@gmail.com>
2019-12-02 15:00:19 +0800

    Fixed collections pages: product details RWD

--- dist/js/myTest.js
+++ dist/js/myTest.js
@@ -28,3 +28,23 @@ $(document).ready(function(){
     centerMode: true,
   });  
 });
+$(window).load(function() {
+  // The slider being synced must be initialized first
+  $('#carousel').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: false,
+    slideshow: false,
+    itemWidth: 210,
+    itemMargin: 5,
+    asNavFor: '#slider'
+  });
+ 
+  $('#slider').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: false,
+    slideshow: false,
+    sync: "#carousel"
+  });
+});
