c0717a5488a7af2068e91c9c572deceb485b723e
Chris oeps <oepschris@gmail.com>
2019-12-02 17:58:27 +0800

    Fixed header style, js

--- dist/js/all.js
+++ dist/js/all.js
@@ -2,7 +2,7 @@ window.onscroll = function () { HeaderFixed() };
 
 //sticky var
 var header = document.getElementById("HeaderFixed");
-var sticky = header.offsetTop;
+//var sticky = header.offsetTop;
 //sticky function
 function HeaderFixed() {
   if (window.pageYOffset > sticky) {
@@ -27,6 +27,38 @@ $(function() {
 	});
 });
 //jQuery Zoom
-$('#product-zoom').zoom();
-});
+// $(function () {
+//   $('#product-zoom').zoom();
+// });
+//
+  // SmartMenus init
+  $(function () {
+    $('#main-menu').smartmenus({
+      subMenusSubOffsetX: 1,
+      subMenusSubOffsetY: -8
+    });
+  });
+
+  // SmartMenus mobile menu toggle button
+  $(function () {
+    var $mainMenuState = $('#main-menu-state');
+    if ($mainMenuState.length) {
+      // animate mobile menu
+      $mainMenuState.change(function (e) {
+        var $menu = $('#main-menu');
+        if (this.checked) {
+          $menu.hide().slideDown(250, function () { $menu.css('display', ''); });
+        } else {
+          $menu.show().slideUp(250, function () { $menu.css('display', ''); });
+        }
+      });
+      // hide mobile menu beforeunload
+      $(window).bind('beforeunload unload', function () {
+        if ($mainMenuState[0].checked) {
+          $mainMenuState[0].click();
+        }
+      });
+    }
+  });
 //jq end
+});
