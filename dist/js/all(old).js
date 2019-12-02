ad08f5fc1976c686f4adf052e81f6f9473e45032
Chris oeps <oepschris@gmail.com>
2019-12-02 19:45:46 +0800

    Renew header link

--- dist/js/all(old).js
+++ dist/js/all(old).js
@@ -1,10 +1,36 @@
+window.onscroll = function () { HeaderFixed() };
+
+//sticky var
+var header = document.getElementById("HeaderFixed");
+//var sticky = header.offsetTop;
+//sticky function
+function HeaderFixed() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
 //jq start
 $(document).ready(function () {
-  //jQuery Zoom
-  // $(function () {
-  //   $('#product-zoom').zoom();
-  // });
-  //
+//search bar
+$("#searchbar .search-label").on("click", function(e){
+  e.preventDefault();
+  $("#searchbar").toggleClass("collapsed");
+});//click
+//
+//
+$(function() {
+	$('#main-menu').smartmenus({
+		subMenusSubOffsetX: 0,
+		subMenusSubOffsetY: -5
+	});
+});
+//jQuery Zoom
+// $(function () {
+//   $('#product-zoom').zoom();
+// });
+//
   // SmartMenus init
   $(function () {
     $('#main-menu').smartmenus({
@@ -34,5 +60,5 @@ $(document).ready(function () {
       });
     }
   });
-  //jq end
+//jq end
 });
